a112d528797e3becacf19166f33fdaeb92efba65
Dmytro Strunin <dmytro.strunin@gmail.com>
2024-06-04 22:43:10 +0200

    Fixing WriteJson

--- cmd/main.go
+++ cmd/main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
 	s3util "github.com/utubun/s3-util"
 )
 
 func main() {
 	s := s3util.New()
-	f, _ := os.Open("simple.csv")
-	stat, _ := f.Stat()
-	buff := make([]byte, stat.Size())
-	f.Read(buff)
-	s.UploadObject("ubot", "simple.csv", buff)
 
 	/* 	err := s.UploadObjectMultipart("ubot", "sample.fastq.gz")
 	   	if err != nil {
 	   		fmt.Println(err)
 	   	} */
 
-	res, _ := s.ListObjectsBucket("ubot")
+	res, err := s.ListObjectsBucket("seqquery-earlyapp", "ubot")
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, val := range res {
 		fmt.Println(*val.Key)
 	}
-
-	_, err := s.SelectObjectContent("ubot", "simple.csv", "")
-	log.Fatal(err)
 }
